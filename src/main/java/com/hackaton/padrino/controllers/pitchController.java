93de9c7b759eb54af6ad3096caebd521bbc00b76
JuanVSDavid <76126640+JuanVSDavid@users.noreply.github.com>
2021-11-21 00:16:06 -0500

    first commit

--- src/main/java/com/hackaton/padrino/controllers/pitchController.java
+++ src/main/java/com/hackaton/padrino/controllers/pitchController.java
@@ -21,6 +21,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -129,5 +130,10 @@ public class pitchController {
             }
         }
         return "redirect:/pitch";
+    }
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable("id") long id){
+        ps.deletePitchById(id);
+        return "redirect:/explorar";
     }
 }
